23bde4e6d91274883ebbcc7ea1a9a460f5b215d9
Kolabank <kolabank@gmail.com>
2022-09-02 23:32:47 +0100

    Able to get form data into js file

--- app.js
+++ app.js
@@ -2,8 +2,14 @@ const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
 const app = express();
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs')
+
 
 const Credentials = require("./models/credential");
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }))
 
 mongoose.connect('mongodb://localhost:27017/credentials', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
@@ -13,3 +19,32 @@ mongoose.connect('mongodb://localhost:27017/credentials', { useNewUrlParser: tru
         console.log("MONGO CONNECTION ERROR!!!!")
         console.log(err)
     })
+
+
+app.get("/", (req, res) => {
+    res.render('index')
+})
+
+app.post("/login", (req, res) => {
+    res.render('login')
+    console.log(req.body);
+    const { username, email, password } = req.body;
+    console.log(`The username is ${username}, with email ${email}`)
+
+
+
+})
+
+app.get("/welcome", (req, res) => {
+    res.send("You are welcome to get response");
+
+})
+
+app.get("/login", (req, res) => {
+    res.render('login');
+
+})
+
+app.listen(3000, () => {
+    console.log("App is listening");
+})
