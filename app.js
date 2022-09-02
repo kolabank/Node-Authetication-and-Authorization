50519cc2ce5c6a27a0e2af714e41fa287e975090
Kolabank <kolabank@gmail.com>
2022-09-03 00:12:43 +0100

    Implemented saving of user credentials to database

--- app.js
+++ app.js
@@ -6,12 +6,12 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs')
 
 
-const Credentials = require("./models/credential");
+const User = require("./models/user");
 
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }))
 
-mongoose.connect('mongodb://localhost:27017/credentials', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb://localhost:27017/user', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log("MONGO CONNECTION OPEN!!!")
     })
@@ -25,13 +25,13 @@ app.get("/", (req, res) => {
     res.render('index')
 })
 
-app.post("/login", (req, res) => {
+app.post("/login", async(req, res) => {
     res.render('login')
-    console.log(req.body);
     const { username, email, password } = req.body;
     console.log(`The username is ${username}, with email ${email}`)
 
-
+    const newUser = new User(req.body);
+    await newUser.save();
 
 })
 
